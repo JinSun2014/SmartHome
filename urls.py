706653d0e3af3050feaf6e530c4a591a8826d082
JinSun <callmesunjplz@gmail.com>
2014-05-11 06:09:00 +0000

    Add static file pattern in url.py

--- urls.py
+++ urls.py
@@ -12,3 +12,7 @@ urlpatterns = patterns('',
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.IndexView.as_view(), name = 'index'),
 )
+
+urlpatterns += patterns('django.contrib.staticfiles.views',
+    url(r'^static/(?P<path>.*)$', 'serve'),
+)
