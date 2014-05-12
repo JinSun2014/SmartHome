eadfcf40db172fea91c96270c7546a6f01f09215
Jin Sun <callmesunjplz@gmail.com>
2014-05-12 06:41:58 +0000

    Change friendly front end button

--- static/js/SmartHome.js
+++ static/js/SmartHome.js
@@ -2,25 +2,29 @@ $(document).ready(function(){
   $('#lamp').click(function(){
     if ($(this).text() == "On"){
       // close lamp
-      $(this).attr('class', 'btn btn-danger col-md-4 col-md-offset-4');
-      //$(this).text("Off");
-      sendCommand('lamp', 'Off', $(this), 'Off');
+      sendCommand('lamp', 'On', $(this));
     }
     else{
       // open lamp
-      $(this).attr('class', 'btn btn-success col-md-4 col-md-offset-4');
-      //$(this).text("On");
-      sendCommand('lamp', 'On', $(this), 'On');
+      sendCommand('lamp', 'Off', $(this));
     }
   });
 });
 
-function sendCommand(device, motion, selector, res){
+function sendCommand(device, motion, selector){
   $.post('operate/' + device + '/' + motion, {
   }, function(response){
     if (response.success){
-      selector.text(res)
+      if (motion == "On"){
+        selector.attr('class', 'btn btn-danger col-md-4 col-md-offset-4');
+        selector.text("Off")
+      }
+      else{
+        selector.attr('class', 'btn btn-success col-md-4 col-md-offset-4');
+        selector.text("On")
+      }
     } else {
     }
   })
 }
+
