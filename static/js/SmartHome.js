58822912b6280cb61d56edd6b78adc9764f21184
Jin Sun <callmesunjplz@gmail.com>
2014-05-12 06:20:54 +0000

    Send json to backend; add ControlView

--- static/js/SmartHome.js
+++ static/js/SmartHome.js
@@ -3,13 +3,24 @@ $(document).ready(function(){
     if ($(this).text() == "On"){
       // close lamp
       $(this).attr('class', 'btn btn-danger col-md-4 col-md-offset-4');
-      $(this).text("Off");
+      //$(this).text("Off");
+      sendCommand('lamp', 'Off', $(this), 'Off');
     }
     else{
       // open lamp
       $(this).attr('class', 'btn btn-success col-md-4 col-md-offset-4');
-      $(this).text("On");
+      //$(this).text("On");
+      sendCommand('lamp', 'On', $(this), 'On');
     }
   });
 });
 
+function sendCommand(device, motion, selector, res){
+  $.post('operate/' + device + '/' + motion, {
+  }, function(response){
+    if (response.success){
+      selector.text(res)
+    } else {
+    }
+  })
+}
